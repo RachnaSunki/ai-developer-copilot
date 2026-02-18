9df5e95186374783bbc3c887b7235e226ee861a3
RachnaSunki <r07043022@gmail.com>
2026-02-18 21:42:19 +0530

    improved response format

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
 from services.openai_service import get_ai_response
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 class ChatRequest(BaseModel):
     message: str
+
+class ChatResponse(BaseModel):
+    success: bool
+    data: Optional[str] = None
+    error: Optional[str] = None
+
     
 @app.get("/")
 def read_root():
     return {"message": "Backend is running"}
 
-@app.post("/chat")
+@app.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
     user_input = request.message
     ai_response = get_ai_response(user_input)
